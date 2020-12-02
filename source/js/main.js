33cf77c32416f769acc1559477b6d4380418ec84
Alyona <markovaa@inbox.ru>
2020-12-02 15:59:06 +0300

    Правит баги по баглисту

--- source/js/main.js
+++ source/js/main.js
@@ -4,6 +4,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/init-modals';
 import {initMenu} from './modules/init-menu';
 import {initVideoPlayBtn} from './modules/init-video-play-btn';
+import moveToAnchor from './modules/move-to-anchor';
 
 //Sliders
 import {initRecipesSlider} from './modules/slider/init-recipes-slider';
@@ -22,6 +23,7 @@ iosVhFix();
 initModals();
 initMenu();
 initVideoPlayBtn();
+moveToAnchor();
 
 // Sliders
 initRecipesSlider();
